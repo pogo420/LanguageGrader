640ea53f51c04daa456d5e7bed4230388fa635ef
Arnab Mukherjee <am0072008@gmail.com>
2023-02-23 10:29:39 +0530

    adding storage interface logic

--- sandwich_go/tests/storageinterface_test.go
+++ sandwich_go/tests/storageinterface_test.go
@@ -27,10 +27,20 @@ func (s1 *St) Delete_sandwich(sandwich ds.Sandwich) ut.Response {
 	return 0
 }
 
-// Unit testcase for checking storage interface
+// Unit testcase for checking sanity of storage interface
 func TestStorageInterface(t *testing.T) {
 	var si_ si.StorageInterface = &St{}
+
 	si_.Setup("oloo")
 
-	// TODO close remaining test cases
+	if si_.Save_sandwich(ds.Sandwich{}) != 0 {
+		t.Fatal("Issue with Save_sandwich logic")
+	} else if si_.Delete_sandwich(ds.Sandwich{}) != 0 {
+		t.Fatal("Issue with Delete_sandwich logic")
+	}
+
+	var sw = si_.Get_sandwich("")
+	if sw.GetName() != "" {
+		t.Fatal("Issue with get sandwich logic")
+	}
 }
